98adb042b067f044d291828595d59ca01579c9da
mkanyar <33469024+mkanyar@users.noreply.github.com>
2023-04-26 14:18:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-kwrq2v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-kwrq2v)
---
+++ dist/async-a.js
@@ -0,0 +1,35 @@
+"use strict";
+(self["webpackChunknode_starter"] = self["webpackChunknode_starter"] || []).push([[794],{
+
+/***/ 956:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ a)
+});
+
+// EXTERNAL MODULE: ./src/d.js
+var d = __webpack_require__(673);
+;// CONCATENATED MODULE: ./src/e.js
+/* harmony default export */ const e = ('e');
+
+// EXTERNAL MODULE: ./src/node_modules/x.js
+var x = __webpack_require__(934);
+// EXTERNAL MODULE: ./src/node_modules/y.js
+var y = __webpack_require__(616);
+;// CONCATENATED MODULE: ./src/a.js
+
+
+
+
+/* harmony default export */ const a = ('a' + d/* default */.Z + e + x/* default */.Z + y/* default */.Z);
+Promise.all(/* import() | async-g */[__webpack_require__.e(714), __webpack_require__.e(137)]).then(__webpack_require__.bind(__webpack_require__, 487));
+
+
+/***/ })
+
+}]);
